cafdffb2c309e129ceb37ff1e278704e20668be8
manomarichal <mano.marichal@student.uantwerpen.be>
2021-08-21 19:54:18 +0200

    small fix to install script

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 # build & start containers
 sudo docker-compose -f docker-compose-dev.yml up -d --build
 
-# create databases and add data
+# create databases
 sudo docker-compose -f docker-compose-dev.yml run teams python manage.py recreate-db
+sudo docker-compose -f docker-compose-dev.yml run leagues python manage.py recreate-db
+sudo docker-compose -f docker-compose-dev.yml run user-interface python manage.py recreate-db
+sudo docker-compose -f docker-compose-dev.yml run referees python manage.py recreate-db
+
+# create databases and add data
 sudo docker-compose -f docker-compose-dev.yml run teams python manage.py add-teams-data
 sudo docker-compose -f docker-compose-dev.yml run teams python manage.py add-clubs-data
 
-sudo docker-compose -f docker-compose-dev.yml run leagues python manage.py recreate-db
 sudo docker-compose -f docker-compose-dev.yml run leagues python manage.py add-divisions-data
 sudo docker-compose -f docker-compose-dev.yml run leagues python manage.py add-status-data
 sudo docker-compose -f docker-compose-dev.yml run leagues python manage.py add-matches-data
 
-sudo docker-compose -f docker-compose-dev.yml run user-interface python manage.py recreate-db
 sudo docker-compose -f docker-compose-dev.yml run user-interface python manage.py add-user-data
 
-sudo docker-compose -f docker-compose-dev.yml run referees python manage.py recreate-db
 sudo docker-compose -f docker-compose-dev.yml run referees python manage.py add-referees-data
+
+# do one final restart
+sudo docker-compose -f docker-compose-dev.yml restart
